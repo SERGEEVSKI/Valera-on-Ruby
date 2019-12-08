6268c3401c6d37e4e97cd24cc3d1ce67d9b8c26d
sergeevski <sergeevski@outlook.com>
2019-12-08 22:46:22 +0700

    Add character's parameters

---
+++ character.rb
@@ -0,0 +1,61 @@
+class Character
+
+	attr_reader :health, :mana, :happiness, :fatigue, :money
+
+	def initialize(health: 0, mana: 0, happiness: 0, fatigue: 0, money: 0)
+		self.health = health
+		self.mana = mana
+		self.fatigue = fatigue
+		self.happiness = happiness
+		self.money = money
+	end
+
+	def dead?
+    	health <= 0 || happiness <= -10
+  	end
+
+	def health=(value)
+    	@health = if value > 100
+				  	100
+				  elsif value.negative?
+				  	0
+				  else
+				  	value
+				  end
+	end
+
+	def mana=(value)
+		@mana = if value > 100
+				  100
+				elsif value.negative?
+				  0
+				else
+				  value
+				end
+	end
+
+	def happiness=(value)
+		@happiness = if value > 10
+						10
+					 elsif value < -10
+					 	-10
+					 else
+					 	value
+					 end
+	end
+
+	def fatigue=(value)
+		@fatigue = if value > 100
+				  100
+				elsif value.negative?
+				  0
+				else
+				  value
+				end
+	end
+
+	def money=(value)
+		@money = value
+	end
+
+end
